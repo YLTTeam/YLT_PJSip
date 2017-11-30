c5c81d4cc27ad71178014ecb99fe1983569afa6b
QTEC <xiangph@qtec.cn>
2017-11-30 16:05:36 +0800

    release 0.0.7

--- YLT_PJSip/Classes/include/pjmedia/rtp_utils.h
+++ YLT_PJSip/Classes/include/pjmedia/rtp_utils.h
@@ -16,6 +16,7 @@
 #ifdef __cplusplus 
 extern "C" {
 #endif
+int pjmedia_key_clear();
 int pjmedia_set_key(unsigned char* keybuf, unsigned int buflen);
 int deal_send(void *input, int data_len, int head_len);
 int deal_receive(void *input, int data_len, int head_len);
